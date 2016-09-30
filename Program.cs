528834f90cad50bbe719e4dfe0534b4c2bba4e37
Erik Faye-Lund <erik@outracks.com>
2016-09-30 20:00:20 +0200

    run shell-commands

--- Program.cs
+++ Program.cs
@@ -50,6 +50,23 @@ namespace AdbTest
             int length = int.Parse(lengthString, System.Globalization.NumberStyles.HexNumber);
             return Encoding.UTF8.GetString(reader.ReadBytes(length));
         }
+
+        public static byte[] ReadAllBytes(this BinaryReader reader)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                var buffer = new byte[4096];
+                while (true)
+                {
+                    int read = reader.Read(buffer, 0, buffer.Length);
+                    if (read <= 0)
+                        break;
+
+                    memoryStream.Write(buffer, 0, read);
+                }
+                return memoryStream.ToArray();
+            }
+        }
     }
 
     class Adb
@@ -65,6 +82,19 @@ namespace AdbTest
         public static byte[] FormatAdbMessage(string message)
         {
             return Encoding.UTF8.GetBytes(string.Format("{0:X4}{1}", message.Length, message));
+        }
+
+        public static Stream TransportSerial(string serial)
+        {
+            var stream = Adb.ConnectToAdb();
+
+            var streamWriter = new BinaryWriter(stream);
+            streamWriter.Write(Adb.FormatAdbMessage("host:transport:" + serial));
+            streamWriter.Flush();
+
+            var streamReader = new BinaryReader(stream);
+            streamReader.Expect("OKAY");
+            return stream;
         }
 
         public static NetworkStream TrackDevices()
@@ -147,7 +177,20 @@ namespace AdbTest
             {
                 var devices = Adb.ListDevices();
                 foreach (var device in devices)
+                {
                     Console.WriteLine(device.ToString());
+                    var stream = Adb.TransportSerial(device.Serial);
+
+                    var writer = new BinaryWriter(stream);
+                    writer.Write(Adb.FormatAdbMessage("shell:ls"));
+                    writer.Flush();
+
+                    var reader = new BinaryReader(stream);
+                    reader.Expect("OKAY");
+
+                    var response = reader.ReadAllBytes();
+                    Console.WriteLine(string.Format("response:\n---8<---\n{0}\n---8<---", Encoding.UTF8.GetString(response)));
+                }
 
                 var deviceListener = new DeviceListener();
                 // deviceListener.DeviceAttached += (object sender, DeviceListener.DeviceEventArgs a) => Console.WriteLine("attached: " + a.Device.ToString());
