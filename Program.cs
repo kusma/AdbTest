3b7d564e00d6e8c8cb44373a92bfb6e7825dbffb
Erik Faye-Lund <erik@outracks.com>
2016-10-27 18:07:38 +0200

    send ping to device

--- Program.cs
+++ Program.cs
@@ -294,6 +294,18 @@ namespace AdbTest
                     Console.WriteLine("attached: " + a.Device.ToString());
                     Console.WriteLine(string.Format("response:\n---8<---\n{0}\n---8<---", Adb.Shell(a.Device, "ls")));
                     Adb.Forward(a.Device, "tcp:1337", "tcp:1337");
+
+                    EndPoint endPoint = new IPEndPoint(IPAddress.Loopback, 1337);
+                    var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    socket.Connect(endPoint);
+                    var stream = new NetworkStream(socket);
+
+                    var streamReader = new StreamReader(stream);
+                    var hello = streamReader.ReadLine();
+                    Console.WriteLine("phone says: {0}", hello);
+                    var streamWriter = new StreamWriter(stream);
+                    streamWriter.WriteLine("HELLO TO YOU TOO, PHONE!\n");
+                    streamWriter.Flush();
                 };
                 deviceListener.DeviceDetached += (object sender, DeviceListener.DeviceEventArgs a) => Console.WriteLine("detached: " + a.Device.ToString());
 
